d3492a51b50f886e1fe66b5fd46a27f4fbdfc994
yassine irab <yassineirab.contact@gmail.com>
2020-02-26 03:51:32 +0100

    Updates

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
+jQuery(document).ready(function () {
+  jQuery(window).scroll(function () {
     // check if scroll event happened
-    if ($(document).scrollTop() > 130) {
+    if (jQuery(document).scrollTop() > 130) {
       // check if user scrolled more than 50 from top of the browser window
-      $(".navbar-change").css("background-color", "#F9F9F9");
+      jQuery(".navbar-change").css("background-color", "#F9F9F9");
       // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
     } else {
-      $(".navbar-change").css("background-color", "transparent");
-      $(".navbar-change").css("transition", "1s");
+      jQuery(".navbar-change").css("background-color", "transparent");
+      jQuery(".navbar-change").css("transition", "1s");
       // if not, change it back to transparent
     }
   });
@@ -22,19 +22,19 @@ $(document).ready(function () {
 
 //Nav Bar File imported to the layout.html from "../Vue/navBar.html"
 
-$(function () {
+jQuery(function () {
 
   "use strict";
-  $("#navbar").load("navigationBar.html");
+  jQuery("#navbar").load("navigationBar.html");
 
 });
 
 
 //footer Bar File imported to the layout.html from "../Vue/footerBar.html"
 
-$(function () {
+jQuery(function () {
 
   "use strict";
-  $("#footerbar").load("footerNavigation.html");
+  jQuery("#footerbar").load("footerNavigation.html");
 
 });
