3fc33d96243da3f176b150db9163eb2757a04808
yassine irab <yassineirab.contact@gmail.com>
2020-02-26 03:45:46 +0100

    Updates

---
+++ component---src-pages-page-2-js-f8377f92af8830ce21b3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{p5nM:function(n,e,t){"use strict";t.r(e);var p=t("q1tI"),a=t.n(p);e.default=function(){return a.a.createElement("p",null,"page 2")}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-f8377f92af8830ce21b3.js.map
---
+++ styles-1008be9930621d7fe407.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"/OlT":function(n,w,o){}}]);
+//# sourceMappingURL=styles-1008be9930621d7fe407.js.map
